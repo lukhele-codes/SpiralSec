1028d94c842a01dd7c4be232c8de25dd47a0f169
bohla <bohla@BOHLALES_HP>
2026-04-11 17:30:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpiralSec
+{//Namespace start
+    public class Program
+    {//Class start
+    static void Main(string[] args)
+    {//Main start 
+    //C
+
+
+    }//Main end
+
+
+
+    }//Class end
+}//Namespace end
